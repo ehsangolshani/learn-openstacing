d216026d426fb1abb79d9d553d2e5a151666f8fb
Ehsan Golshani <ehsan.golshani@snapp.cab>
2019-02-06 12:30:02 +0330

    get value of LOCAL_AGENT_HOST_PORT and COLLECTOR_HOST_PORT from env

--- hello.go
+++ hello.go
@@ -8,26 +8,33 @@ import (
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 	"io"
+	"os"
 	"time"
 )
 
 // initJaeger returns an instance of Jaeger Tracer that samples 100% of traces and logs all spans to stdout.
 func initJaeger(service string) (opentracing.Tracer, io.Closer) {
+
+	localAgentHostPort := os.Getenv("LOCAL_AGENT_HOST_PORT")
+	collectorHostPort := os.Getenv("COLLECTOR_HOST_PORT")
+
 	cfg := &config.Configuration{
+		ServiceName: service,
 		Sampler: &config.SamplerConfig{
 			Type:  "const",
 			Param: 1,
 		},
 		Reporter: &config.ReporterConfig{
-			LogSpans: true,
+			LogSpans:           true,
+			LocalAgentHostPort: localAgentHostPort,
+			CollectorEndpoint:  collectorHostPort,
 		},
 	}
-	tracer, closer, err := cfg.New(service, config.Logger(jaeger.StdLogger))
+
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
 	}
-
-
 
 	return tracer, closer
 }
@@ -53,7 +60,7 @@ func main() {
 		span.Finish()
 		_ = closer.Close()
 
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(10000 * time.Millisecond)
 	}
 }
 
